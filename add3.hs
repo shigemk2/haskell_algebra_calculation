eda1a4fb8cbcbc760cc0b46e65ada0d687442ca6
shigemk2 <i.am.shige@gmail.com>
2015-02-19 23:54:22 +0900

    リスト対応

--- add3.hs
+++ add3.hs
@@ -3,13 +3,13 @@ module Main where
 import Test.HUnit
 import System.IO
 
-data Add = Add Int Int Int deriving Show
+data Add = Add [Int] deriving Show
 
-eval (Add x y z) = x + y + z
+eval (Add xs) = sum xs
 
 tests = TestList
-    [ "eval 1" ~: eval (Add 1 1 1) ~?= 1+1+1
-    , "eval 2" ~: eval (Add 2 3 3) ~?= 2+3+3
+    [ "eval 1" ~: eval (Add [1, 1]) ~?= 1+1
+    , "eval 2" ~: eval (Add [2, 3]) ~?= 2+3
     ]
 
 main = do
