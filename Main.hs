53915448e1e5fd23d6abef66a15bbe29da7cad78
shigemk2 <i.am.shige@gmail.com>
2015-02-22 15:35:59 +0900

    係数

--- Main.hs
+++ Main.hs
@@ -4,7 +4,7 @@ import Test.HUnit
 import System.IO
 
 data Expr = N   Int
-          | Var String Int
+          | Var String Int Int
           | Add [Expr]
           | Mul [Expr]
           deriving (Show, Eq)
@@ -13,8 +13,9 @@ eval (N x ) = x
 eval (Add xs) = sum [eval x | x <- xs]
 eval (Mul xs) = product [eval x | x <- xs]
 
-isneg (N n) | n < 0 = True
-isneg _ = False
+isneg (N n)       | n < 0 = True
+isneg (Var _ a _) | a < 0 = True
+isneg _                   = False
 
 str (N x) = show x
 str (Add [])       = ""
@@ -28,9 +29,11 @@ str (Mul [Add xs]) = "(" ++ str (Add xs) ++ ")"
 str (Mul [Mul xs]) = "(" ++ str (Mul xs) ++ ")"
 str (Mul [x])      = str x
 str (Mul (x:xs))   = str (Mul [x]) ++ "*" ++ str (Mul xs)
-str (Var x 1)      = x
-str (Var x n)      = x ++ "^" ++ show n
-x n                = Var "x" n
+str (Var x  1 1) = x
+str (Var x(-1)1) = "-" ++ x
+str (Var x  a 1) = show a ++ x
+str (Var x  a n) = str (Var x a 1) ++ "^" ++ show n
+x a n = Var "x" a n
 
 tests = TestList
         [ "eval 1" ~: eval (Add[N 1,N 1 ]) ~?= 1+1
@@ -47,8 +50,10 @@ tests = TestList
         , "str 6" ~: str (Mul[Add[N 1,N 2],N 3]) ~?= "(1+2)*3"
         , "str 7" ~: str (Mul[Mul[N 1,N 2],N 3]) ~?= "(1*2)*3"
         , "equal" ~: Add[N 1,N 2] ~?= Add[N 1,N 2]
-        , "x 1" ~: str (Add [x 1,N 1]) ~?= "x+1"
-        , "x 2" ~: str (Add [x 2,x 1,N 1]) ~?= "x^2+x+1"
+        -- , "x 1" ~: str (Add [x 1,N 1]) ~?= "x+1"
+        -- , "x 2" ~: str (Add [x 2,x 1,N 1]) ~?= "x^2+x+1"
+        , "x 1" ~: str (Add [1`x`1,N 1]) ~?= "x+1"
+        , "x 2" ~: str (Add [1`x`3,(-1)`x`2,(-2)`x`1,N 1]) ~?= "x^3-x^2-2x+1"
         ]
 
 main = do
